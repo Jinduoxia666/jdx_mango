b9d3042fcea0d12f38be43d43f6f2e2c541ee351
Jinduoxia666 <yss1994418@outlook.com>
2020-01-04 14:28:49 +0800

    修改项目名称 2019-1-4

---
+++ mango-admin/src/main/java/com/louis/admin/mango/service/SysUserService.java
@@ -0,0 +1,11 @@
+package com.louis.admin.mango.service;
+
+
+import com.louis.admin.mango.model.SysUser;
+
+import java.util.List;
+
+public interface SysUserService {
+
+    List<SysUser> findAll();
+}
